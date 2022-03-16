ba4c002cf0b39e2001af21f56acc2b290b3e5e91
Elizabeth <elizabeth.por@gmail.com>
2022-03-16 11:30:19 -0500

    commit tree a2f5a416ab701966bc6de7d700b794ced18a6b2e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_reader :length1, :length2, :length3
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+
+  end
+
+  def kind
+    if length1 == length2 && length2 == length3
+      :equilateral
+    elsif (length1 != length2 && length2 == length3) || (length1 == length2 && length2 != length3) || (length1 == length3 && length2 != length3)
+      :isosceles
+    elsif length1 != length2 && length2 != length3
+      :scalene
+    end
+  end
+
 end
